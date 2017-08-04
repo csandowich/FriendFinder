c83e1962f2ba7cf4c95daeca8918b50640aa4615
Caleb Sandowich <calebsandowich@Calebs-MacBook-Pro.local>
2017-08-03 21:25:04 -0700

    finished html and added some paths get paths in server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+  type: "application/vnd.api+json"
+}));
+
+app.get("/", function(req, res) {
+  console.log("route /");
+  res.sendFile(path.join(__dirname, "/app/public/home.html"));
+});
+app.get("/survey", function(req, res) {
+  console.log("route /");
+  res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+});
+
+app.listen(3000);
